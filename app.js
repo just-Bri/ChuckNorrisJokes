8b6ff1a78dab49030a805c606b77b1dd7a7857b4
Bridger <bridgerrhammond@gmail.com>
2019-10-03 08:04:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function getJokes(e) {
   console.log(number);
 
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.open("GET", `https://api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function() {
     if (this.status === 200) {
